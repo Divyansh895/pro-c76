69e44ce3b7017f59a4c304a00dfa85208e33af96
Divyansh Patel <divyansh234patel@gmail.com>
2024-02-27 18:50:52 +0530

    first commit

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import {NavigationContainer} from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from "./screens/Home";
 import IssLocationScreen from "./screens/IssLocation";
 import MeteorScreen from "./screens/Meteors";
@@ -15,6 +16,25 @@ export default function App() {
   );
 }
 
+const Stack = createStackNavigator();
+
+function App(){
+  return(
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Home' screenOption={{
+        headerShown: false
+      }}>
+        <Stack.Screen name="Home" component={HomeScreen}/>
+        <Stack.Screen name='IssLocation' component={IssLocationScreen}/>
+        <Stack.Screen name='Meteros' component={MeteorScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
+
+
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -23,3 +43,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
+
